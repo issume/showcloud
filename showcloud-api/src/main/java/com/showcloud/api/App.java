a626c1409f1af6348a393feb365a9dfa5d5d46c1
hym <123053874@qq.com>
2018-04-25 09:20:36 +0800

    项目结构

---
+++ showcloud-api/src/main/java/com/showcloud/api/App.java
@@ -0,0 +1,13 @@
+package com.showcloud.api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
